b6db9a5914db97e703dd6e1eaa615ccbb4e46668
RadithSandeepa <radithsandeepa500@gmail.com>
2024-08-05 16:41:50 +0530

    Updated videocard component logic & design

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,8 +1,11 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { icons } from '../constants'
 
 const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avatar }} }) => {
+
+  const [play, setPlay] = useState(false);
+
   return (
     <View className="flex-col items-center px-4 mb-14">
       <View className="flex-row gap-3 items-start">
@@ -28,6 +31,22 @@ const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avat
             />
         </View>
       </View>
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity activeOpacity={0.7} onPress={() => setPlay(true)} className="w-full h-60 rounded-xl mt-3 relative justify-center items-center">
+          <Image
+            source={{ uri: thumbnail}}
+            className="w-full h-full rounded-xl mt-3"
+            resizeMode='cover'
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode='contain'
+          />
+        </TouchableOpacity>
+      )}
     </View>
   )
 }
